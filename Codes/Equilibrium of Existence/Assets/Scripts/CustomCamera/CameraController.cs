ce2a36fffdce919a2dc4f13462a921f0bdfff29d
Görkem Küpeli <mgorkemkupeli@gmail.com>
2020-12-20 00:00:04 +0300

    Küçük değişiklikler.

---
+++ Codes/Equilibrium of Existence/Assets/Scripts/CustomCamera/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace CustomCamera
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private Transform player;
+        [SerializeField] private float smoothingSpeed = 12.5f;
+        [SerializeField] private Vector3 offset = new Vector3 (0f,0f,-10f);
+
+        private void Start()
+        {
+            transform.position = player.position + offset;
+        }
+        private void Update()
+        {
+            var desiredPosition = player.position + offset;
+            var smoothedPosition = Vector3.Lerp(transform.position,desiredPosition,smoothingSpeed * Time.deltaTime);
+            transform.position = smoothedPosition;
+        }
+    }
+}
