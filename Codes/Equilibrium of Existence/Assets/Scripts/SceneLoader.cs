3ea1e8d3029c6eb93e30ce3a48e13d85d9fb59d6
ezoisil <e.ezoozcelik@gmail.com>
2020-12-19 20:26:59 +0300

    Main Menu

---
+++ Codes/Equilibrium of Existence/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    int currentSceneIndex;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = 0;
+    }
+
+    public void SceneLoaderFunc(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void MainMenuLoader()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadNextScene()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
