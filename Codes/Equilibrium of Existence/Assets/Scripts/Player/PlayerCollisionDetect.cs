06be084acc36dba62e485518f72fd34861a14185
jadesage <70180843+jadesage@users.noreply.github.com>
2020-12-20 00:23:09 +0300

    Collision
    Detection co routine stop input

--- Codes/Equilibrium of Existence/Assets/Scripts/Player/PlayerCollisionDetect.cs
+++ Codes/Equilibrium of Existence/Assets/Scripts/Player/PlayerCollisionDetect.cs
@@ -7,13 +7,44 @@ public class PlayerCollisionDetect : MonoBehaviour
 {
     [SerializeField] private GameObject Player;
     [SerializeField] private GameObject Obstacle;
+    bool isCoroutine = false;
+    private Coroutine coroutine;
+    
 
      void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Collision");
-        LevelManager.ResetLevel();
+        if (isCoroutine == false)
+        {
+            LevelManager.ResetLevel();
+            Debug.Log("Restarted");
+        }
     }
-    
-        
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isCoroutine == true)
+            {
+                StopCoroutine(coroutine);
+                Debug.Log("Stopped");
+                isCoroutine = false;
+                coroutine = StartCoroutine(PlayerCoroutine());
+            }
+            else
+            {
+                
 
+                coroutine = StartCoroutine(PlayerCoroutine());
+            }
+        }
+    }
+    IEnumerator PlayerCoroutine()
+    {
+        Debug.Log("Coroutine start");
+        isCoroutine = true;
+        yield return new WaitForSeconds(2);
+        isCoroutine = false;
+        Debug.Log("Couroutine finish");
+    }
 }
