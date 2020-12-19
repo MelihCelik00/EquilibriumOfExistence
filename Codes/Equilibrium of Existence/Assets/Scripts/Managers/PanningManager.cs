1deedd400438eedc93e61be6b6a4d2c871e13eb3
MelihCelik00 <melihsafa.c@gmail.com>
2020-12-20 01:46:20 +0300

    Merge conflict fix

--- Codes/Equilibrium of Existence/Assets/Scripts/Managers/PanningManager.cs
+++ Codes/Equilibrium of Existence/Assets/Scripts/Managers/PanningManager.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class PanningManager : MonoBehaviour
+namespace Managers
 {
-    private Slider _slider;
-    private AudioSource _audioSource;
-    private void Start()
+    public class PanningManager : MonoBehaviour
     {
-        _slider = FindObjectOfType<Slider>();
-        _audioSource = FindObjectOfType<AudioSource>();
-    }
+        private Slider _slider;
+        private AudioSource _audioSource;
+        private void Start()
+        {
+            _slider = FindObjectOfType<Slider>();
+            _audioSource = FindObjectOfType<AudioSource>();
+        }
 
-    private void Update()
-    {
-        _audioSource.panStereo = 1 * _slider.value;
+        private void Update()
+        {
+            _audioSource.panStereo = 1 * _slider.value;
+        }
     }
 }
