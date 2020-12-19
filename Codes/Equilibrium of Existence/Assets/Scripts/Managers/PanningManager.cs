21d318470d997101e936c5495a607fa4517d0ba0
MelihCelik00 <melihsafa.c@gmail.com>
2020-12-20 00:34:14 +0300

    Audio panning added

---
+++ Codes/Equilibrium of Existence/Assets/Scripts/Managers/PanningManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanningManager : MonoBehaviour
+{
+    private Slider _slider;
+    private AudioSource _audioSource;
+    private void Start()
+    {
+        _slider = FindObjectOfType<Slider>();
+        _audioSource = FindObjectOfType<AudioSource>();
+    }
+
+    private void Update()
+    {
+        _audioSource.panStereo = 1 * _slider.value;
+    }
+}
